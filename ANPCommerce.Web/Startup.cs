31a4c3c0ad2d638836b39c736219ea7b31978aad
Milton Câmara <miltomcamara@gmail.com>
2019-09-19 23:43:58 -0300

    Adicionado Application Inisghts

--- ANPCommerce.Web/Startup.cs
+++ ANPCommerce.Web/Startup.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ANPCommerce.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using ANPCommerce.Web.Models;
 
 namespace ANPCommerce.Web
 {
@@ -33,11 +28,9 @@ namespace ANPCommerce.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            services.AddDbContext<ANPCommerceWebContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ANPCommerceWebContext")));
+            services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddDbContext<ANPCommerceWebContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ANPCommerceWebContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
